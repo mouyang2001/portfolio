578b2f3c999af5ce9a8ef7adf5038c44ccca5712
Matthew Ouyang <43261675+mouyang2001@users.noreply.github.com>
2021-08-08 17:17:38 +1200

    fix fetch url

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'portfolio/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
